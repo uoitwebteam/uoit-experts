d740a3d31b63b5dbf2e272ed6312734f8eea0868
Wose <Jacksonteather@hotmail.com>
2017-06-13 01:16:39 -0400

    🛠 WIP add search controls to controls service

--- src/app/controls/controls.service.ts
+++ src/app/controls/controls.service.ts
@@ -7,11 +7,17 @@ import { Industry } from '../models';
 @Injectable()
 export class ControlsService {
 	
+  private searchControlsSource = new Subject<string>();
   private sortControlsSource = new Subject<string>();
   private filterControlsSource = new Subject<Industry[]>();
 
+  searchControls$ = this.sortControlsSource.asObservable();
   sortControls$ = this.sortControlsSource.asObservable();
   filterControls$ = this.filterControlsSource.asObservable();
+
+  search(search: string) {
+    this.searchControlsSource.next(search);
+  }
 
   sort(sort: string) {
     this.sortControlsSource.next(sort);
